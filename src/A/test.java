e049da8b285a09c9429e6e661eb150a84636c32d
seongjinkim043 <seongjinkim043@gmail.com>
2025-03-17 13:06:57 +0900

    test1

---
+++ src/A/test.java
@@ -0,0 +1,34 @@
+package A;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner insert = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = insert.nextInt();
+
+        // 위쪽 삼각형 (피라미드)
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n - a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= a * 2 - 1; c++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래쪽 삼각형 (역피라미드)
+        for (int a = n - 1; a >= 1; a--) {
+            for (int b = 1; b <= n - a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= a * 2 - 1; c++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        insert.close(); // Scanner 닫기
+    }
+}
